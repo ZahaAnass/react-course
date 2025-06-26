9bc450f3a3560c4945c462ed23b3b5105e9c288c
ZahaAnass <zahaanass277@gmail.com>
2025-06-26 17:29:20 +0100

    add color picker app

---
+++ 10-color-picker-app/src/App.jsx
@@ -0,0 +1,16 @@
+// onChange = event handler used primarily with form elements
+//            ex. <input>, <textarea>, <select>, radio buttons, checkboxes
+//            Triggers a function every time the value of the input changes
+
+import ColorPicker from "./ColorPicker"
+
+function App(){
+
+  return(
+    <>
+      < ColorPicker />
+    </>
+  )
+}
+
+export default App
