a7bfdd329e4bef77a770b066b946ba4501fc44b6
ZahaAnass <zahaanass277@gmail.com>
2025-06-26 14:43:49 +0100

    add useState hook

---
+++ 8-useState hook/src/Counter.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from "react"
+
+function Counter(){
+
+    const [count, setCount] = useState(0);
+
+    const incrementCount = () => {
+        setCount(count + 1);
+    }
+
+    const decrementCount = () => {
+        setCount(count - 1);
+    }
+
+    const resetCount = () => {
+        setCount(0);
+    }
+
+    return(
+        <div className="counter-container">
+            <p className="count-display">{count}</p>
+            <button className="counter-button" onClick={decrementCount}>Decrement</button>
+            <button className="counter-button" onClick={resetCount}>Reset</button>
+            <button className="counter-button" onClick={incrementCount}>Increment</button>
+        </div>
+    )
+
+}
+
+export default Counter
